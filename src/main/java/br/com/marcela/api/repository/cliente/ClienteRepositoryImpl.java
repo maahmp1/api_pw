1b28d7b184ea44e79be0e3df623b64b46691a5bb
maahmp1 <marcelamartinspena@gmail.com>
2023-06-26 09:49:22 -0300

    finished: cliente repository impl

--- src/main/java/br/com/marcela/api/repository/cliente/ClienteRepositoryImpl.java
+++ src/main/java/br/com/marcela/api/repository/cliente/ClienteRepositoryImpl.java
@@ -2,13 +2,20 @@ package br.com.marcela.api.repository.cliente;
 
 import br.com.marcela.api.model.Cliente;
 import br.com.marcela.api.repository.filter.ClienteFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteRepositoryImpl implements ClienteRepositoryQuery {
 
@@ -21,9 +28,53 @@ public class ClienteRepositoryImpl implements ClienteRepositoryQuery {
         CriteriaBuilder builder = manager.getCriteriaBuilder();
         CriteriaQuery<Cliente> criteria = builder.createQuery(Cliente.class);
         Root<Cliente> root = criteria.from(Cliente.class);
-       // parei aqui
-        
 
-        return null;
+        Predicate[] predicates = criarRestricoes(clienteFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecliente")));
+
+        TypedQuery<Cliente> query = manager.createQuery(criteria);
+
+        adicionarRestricoesDePaginacao(query, pageable);
+
+        return new PageImpl<>(query.getResultList(), pageable, totaldepaginas(clienteFilter));
+    }
+
+    private Long totaldepaginas(ClienteFilter clienteFilter){
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Cliente> root = criteria.from(Cliente.class);
+
+        Predicate[] predicates = criarRestricoes(clienteFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecliente")));
+
+        criteria.select(builder.count(root));
+
+        return manager.createQuery(criteria).getSingleResult();
+    }
+
+    private void adicionarRestricoesDePaginacao(TypedQuery<Cliente> query, Pageable pageable) {
+
+        int paginaAtual = pageable.getPageNumber();
+        int totalDeRegistrosPorPagina = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginaAtual * totalDeRegistrosPorPagina;
+
+        query.setFirstResult(primeiroRegistroDaPagina);
+        query.setMaxResults(totalDeRegistrosPorPagina);
+
+    }
+
+    private Predicate[] criarRestricoes(ClienteFilter clienteFilter, CriteriaBuilder builder, Root<Cliente> root) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (!StringUtils.isEmpty(clienteFilter.getNomecliente())){
+            predicates.add(builder.like(builder.lower(root.get("nomecliente")),
+                    "%" + clienteFilter.getNomecliente().toLowerCase() + "%" ));
+        }
+
+
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 }
--- src/main/java/br/com/marcela/api/repository/filter/ContasAReceberFilter.java
+++ src/main/java/br/com/marcela/api/repository/filter/ContasAReceberFilter.java
@@ -1,4 +1,9 @@
 package br.com.marcela.api.repository.filter;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class ContasAReceberFilter {
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+
 }
